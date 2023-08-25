3c1f4d15e2b72f17fc0ae4d41c90ec70f8ee9b8e
Roger Crew <crew@cs.stanford.edu>
2023-08-25 16:50:51 -0700

    unicode_library_version
    libraries should identify themselves.
    
    Also, libucd.3 described 'unicode_character_version'
    which is not actually defined -> removed; + other trivial edits.

--- ucdversion.pl
+++ ucdversion.pl
@@ -1,14 +1,36 @@
 #!/usr/bin/perl
+
+@vinfo = ();
+
 open(UCDVERSION, '<', 'ucd/version') or die;
 $line = <UCDVERSION>;
 chomp $line;
 close(UCDVERSION);
 
-@v = split(/\./, $line);
+push @vinfo, unicode_database_version => $line;
+
+open(LIBVERSION,'-|','git describe --tags') or die;
+<LIBVERSION> =~ m/libucd-([.0-9]+)-/;
+close(LIBVERSION);
+
+push @vinfo, unicode_library_version => $1;
+
 open(GV, '>', 'gen/ucdversion.c') or die;
 print  GV "#include \"ucd.h\"\n";
-print  GV "int unicode_database_version(void)\n";
-print  GV "{\n";
-printf GV "\treturn 0x%x;\n", ($v[0] << 16)+($v[1] << 8)+$v[2];
-print  GV "}\n";
+
+while (@vinfo) {
+
+    my $fname = shift @vinfo;
+    my @v = split(/\./, shift @vinfo);
+
+    printf  GV <<'END' , $fname, ($v[0] << 16)+($v[1] << 8)+$v[2];
+
+int %s(void)
+{
+	return 0x%x;
+}
+END
+
+}
+
 close(GV);
